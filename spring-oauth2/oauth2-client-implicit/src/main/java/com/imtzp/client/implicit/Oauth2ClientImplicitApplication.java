731ec5110cabba34ac0ac0aa8b3b4bfa42abe453
logan2013 <lo-gan@qq.com>
2016-10-08 17:16:02 +0800

    OAuth2 implicit授权方式

---
+++ spring-oauth2/oauth2-client-implicit/src/main/java/com/imtzp/client/implicit/Oauth2ClientImplicitApplication.java
@@ -0,0 +1,12 @@
+package com.imtzp.client.implicit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientImplicitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientImplicitApplication.class, args);
+	}
+}
---
+++ spring-oauth2/oauth2-client-implicit/src/main/java/com/imtzp/client/implicit/WebConfig.java
@@ -0,0 +1,16 @@
+package com.imtzp.client.implicit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index");
+		registry.addViewController("/index");
+		registry.addViewController("/oauthTemp");
+	}
+}
