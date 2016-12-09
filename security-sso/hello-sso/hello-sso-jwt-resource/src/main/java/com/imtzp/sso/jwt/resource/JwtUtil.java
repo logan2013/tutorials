b05605816c4bdcb3c7f179ef50fb05188d946d73
laogang <laogang@imtzp.com>
2016-12-09 13:57:02 +0800

    使用jwt实现简单的sso登录，不包含spring security

---
+++ security-sso/hello-sso/hello-sso-jwt-resource/src/main/java/com/imtzp/sso/jwt/resource/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.imtzp.sso.jwt.resource;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtil {
+
+	public static String generateToken(String signingKey, String subject) {
+		long nowMills = System.currentTimeMillis();
+		Date now = new Date(nowMills);
+
+		JwtBuilder builder = Jwts.builder().setSubject(subject).setIssuedAt(now).signWith(SignatureAlgorithm.HS256,
+				signingKey);
+
+		return builder.compact();
+	}
+
+	public static String getSubject(HttpServletRequest httpServletRequest, String jwtTokenCookieName,
+			String signingKey) {
+		String token = CookieUtil.getValue(httpServletRequest, jwtTokenCookieName);
+
+		if (token == null) {
+			return null;
+		}
+
+		return Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody().getSubject();
+	}
+}
