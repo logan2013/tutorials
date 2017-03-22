6b179eeef49e55b1d520a7bccdf9fc57a1fef5e4
laogang <laogang@imtzp.com>
2017-03-22 15:52:12 +0800

    在sping boot启动时运行代码

---
+++ spring-boot/spring-boot-hook-runner/src/main/java/com/imtzp/hook/AppStartupRunner.java
@@ -0,0 +1,18 @@
+package com.imtzp.hook;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupRunner implements ApplicationRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AppStartupRunner.class);
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.info("Your application started with option names : {}", args.getOptionNames());
+	}
+}
