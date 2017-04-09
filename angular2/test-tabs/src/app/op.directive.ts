70977c8f28eb01a148e2a4f0ea0b3a5a889b264d
logan2013 <lo-gan@qq.com>
2017-04-09 22:25:53 +0800

    tab动态加载组件

--- angular2/test-tabs/src/app/op.directive.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, ViewContainerRef} from '@angular/core';
-
-@Directive({
-  selector: '[ad-host]',
-})
-export class OpDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {
-  }
-}
--- angular2/test-tabs/src/app/tab-item-data.ts
+++
@@ -1,7 +0,0 @@
-import {Type} from '@angular/core';
-export class TabItemData {
-
-  constructor(public title: string, public component: Type<any>,  public data: any, public activated: boolean = true) {
-
-  }
-}
